5fe085512711b799d77131d52a4a7f6fc9e0871c
Ralph Glass <ralph.glass@t-online.de>
2013-03-09 22:33:38 +0100

    create new windows through gtk (fork removed), javascript now optional, manpage added

--- web-1.0/web.c
+++ web-1.0/web.c
@@ -20,6 +20,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int               window_count = 0;
+static gboolean          javascript = FALSE;
+static WebKitWebSettings *settings;
+
 /* G_CALLBACK(functions) */
 
 static void hoveringOverLink( WebKitWebView* webView ,
@@ -33,7 +37,9 @@ static void hoveringOverLink( WebKitWebView* webView ,
 
 static void destroy( GtkWidget* widget, GtkWidget* window )
 {
-        gtk_main_quit();
+        if (window_count < 2)
+                gtk_main_quit();
+        window_count--;
 }
 
 static void closeView( WebKitWebView* webView, GtkWidget* window )
@@ -52,19 +58,19 @@ static void goHome( GtkWidget* window, WebKitWebView* webView )
         webkit_web_view_load_uri( webView, "http://www.google.com" ); 
 }
 
-static void downloadRequested( WebKitWebView*   webView ,
-                                WebKitDownload* download ,
-                                GtkEntry*       user_data_entry )
+static void downloadRequested( WebKitWebView*  webView,
+                               WebKitDownload* download, 
+                               GtkEntry*       user_data_entry )
 {
         const char* uri = gtk_entry_get_text( user_data_entry );
         const char* homedir = getenv("HOME");
-        const char* downloaddir = g_strjoin( NULL ,   
-                                             "--directory-prefix=" ,
-                                             homedir , 
-                                             "/Downloads" , 
+        const char* downloaddir = g_strjoin( NULL,   
+                                             "--directory-prefix=",
+                                             homedir, 
+                                             "/Downloads", 
                                              NULL );
         /* fork download with wget */
-        int r= 0;
+        int r = 0;
         r = fork();
         if (r>0) {
                 execl( "/usr/bin/wget", "wget", downloaddir, // download directory
@@ -75,14 +81,19 @@ static void downloadRequested( WebKitWebView*   webView ,
                                                 NULL); };
 }
 
-static void searchText( WebKitWebView* webView,
-                        gchar*         searchString )
+static void searchText( WebKitWebView* webView, gchar* searchString )
 {
-        
         webkit_web_view_unmark_text_matches( webView );
         webkit_web_view_search_text( webView, searchString, false, true, true );
         webkit_web_view_mark_text_matches( webView, searchString, false, 0 );
         webkit_web_view_set_highlight_text_matches( webView, true );
+}
+
+static void toggleJavascript( GtkWidget* window , WebKitWebView* webView )
+{
+        javascript=!javascript;
+        g_object_set (G_OBJECT(settings), "enable-scripts", javascript , NULL);
+        webkit_web_view_reload( webView );
 }
 
 static void activateEntry( GtkWidget* entry, gpointer gdata )
@@ -111,28 +122,16 @@ static void activateEntry( GtkWidget* entry, gpointer gdata )
         webkit_web_view_load_uri( webView, uri );
 }
 
-static WebKitWebView* createWebView ( WebKitWebView*  webView,
+static WebKitWebView* createWebView ( WebKitWebView*  parentWebView,
                                       WebKitWebFrame* frame,
-                                      GtkEntry*       uriEntry)
+                                      GtkEntry*       parentUriEntry)
 {
+        window_count++;
         const char* user_data;
-        user_data = gtk_entry_get_text( uriEntry );
-        /* fork new browser */
-        int r = 0;
-        r = fork();
-        if ( r > 0 ) {
-                execl( "/usr/bin/web", "web" , user_data , "&" , NULL ); }
-        else {
-                return NULL; }
-}
-
-/* main */
-
-int main( int argc, char* argv[] )
-{
-        gtk_init( &argc, &argv );
-
-        GtkWidget*     main_window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
+        if (!parentUriEntry == 0)
+                user_data = gtk_entry_get_text( parentUriEntry );
+        
+        GtkWidget*     window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
         GtkWidget*     vbox = gtk_vbox_new( FALSE, 0 );
         GtkWidget*     scrolledWindow = gtk_scrolled_window_new( NULL, NULL );
         GtkWidget*     uriEntry = gtk_entry_new();
@@ -140,39 +139,62 @@ int main( int argc, char* argv[] )
         GtkToolItem*   item;
         WebKitWebView* webView = WEBKIT_WEB_VIEW( webkit_web_view_new() );
 
+        g_object_set (G_OBJECT(settings), "enable-scripts", javascript, NULL); 
+        g_object_set (G_OBJECT(settings), "enable-private-browsing", TRUE, NULL);
+        webkit_web_view_set_settings( WEBKIT_WEB_VIEW(webView), settings);
+
         gtk_entry_set_text( GTK_ENTRY( uriEntry ), "http://" );
 
         item = gtk_tool_button_new_from_stock( GTK_STOCK_GO_BACK );
+        gtk_tool_button_set_label(GTK_TOOL_BUTTON( item ), "Back" );
         gtk_toolbar_insert( GTK_TOOLBAR( toolbar ), item, -1 );
         g_signal_connect( G_OBJECT( item ), "clicked", G_CALLBACK( goBack ), webView );
 
         item = gtk_tool_button_new_from_stock( GTK_STOCK_HOME );
         gtk_toolbar_insert( GTK_TOOLBAR( toolbar ), item, -1 );
+        gtk_tool_button_set_label(GTK_TOOL_BUTTON( item ), "Home" );
         g_signal_connect( G_OBJECT( item ), "clicked", G_CALLBACK( goHome ), webView );
 
-        gtk_window_set_default_size( GTK_WINDOW( main_window ), 1024, 768 );
+        item = gtk_toggle_tool_button_new_from_stock( GTK_STOCK_PROPERTIES );
+        gtk_tool_button_set_label(GTK_TOOL_BUTTON( item ), "Javascript" );
+        gtk_toolbar_insert( GTK_TOOLBAR( toolbar ), item, -1);
+        gtk_toggle_tool_button_set_active( GTK_TOGGLE_TOOL_BUTTON( item ), javascript );
+        g_signal_connect( G_OBJECT( item ), "toggled", G_CALLBACK( toggleJavascript ), webView );
+
+        gtk_window_set_default_size( GTK_WINDOW( window ), 1024, 768 );
         g_object_set_data( G_OBJECT( uriEntry ), "webView", webView );
 
         gtk_container_add(  GTK_CONTAINER( scrolledWindow ), GTK_WIDGET( webView ) );
         gtk_box_pack_start( GTK_BOX( vbox ), toolbar, FALSE, FALSE, 0 );
         gtk_box_pack_start( GTK_BOX( vbox ), uriEntry, FALSE, FALSE, 0 );
         gtk_box_pack_start( GTK_BOX( vbox ), scrolledWindow, TRUE, TRUE, 0 );
-        gtk_container_add(  GTK_CONTAINER( main_window ), vbox );
+        gtk_container_add(  GTK_CONTAINER( window ), vbox );
 
-        g_signal_connect( main_window, "destroy",             G_CALLBACK( destroy ),            NULL );
-        g_signal_connect( webView ,    "close-web-view",      G_CALLBACK( closeView ),          main_window );
+        g_signal_connect( window,      "destroy",             G_CALLBACK( destroy ),            window );
+        g_signal_connect( webView ,    "close-web-view",      G_CALLBACK( closeView ),          window );
         g_signal_connect( uriEntry,    "activate",            G_CALLBACK( activateEntry ),      NULL );
         g_signal_connect( webView ,    "create-web-view",     G_CALLBACK( createWebView ),      uriEntry );
         g_signal_connect( webView ,    "hovering_over_link",  G_CALLBACK( hoveringOverLink ),   uriEntry );
-        g_signal_connect( webView ,    "download-requested" , G_CALLBACK( downloadRequested ), uriEntry );
+        g_signal_connect( webView ,    "download-requested" , G_CALLBACK( downloadRequested ),  uriEntry );
 
-        if (!argv[1] == 0)
-                webkit_web_view_load_uri( webView, argv[1] );
+        g_assert( user_data );
+        if (!user_data)
+                webkit_web_view_load_uri( webView, user_data );
         else
                 webkit_web_view_load_uri( webView, "http://www.google.com" );
 
         gtk_widget_grab_focus( GTK_WIDGET( webView ) );
-        gtk_widget_show_all( main_window );
+        gtk_widget_show_all( window );
+        return webView;
+}
+
+/* main */
+
+int main( int argc, char* argv[] )
+{
+        gtk_init( &argc, &argv );
+        settings = webkit_web_settings_new();
+        createWebView( NULL, NULL, NULL );
         gtk_main();
         return 0;
 }
